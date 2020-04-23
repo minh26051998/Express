f82fba64792c0e8240d3b293e9b9671ee4defffa
CongMinh <minh260519988@gmail.com>
2020-04-23 07:54:04 +0700

    no problem

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
-const low = require('lowdb');
-const FileSync = require('lowdb/adapters/FileSync');
-const adapter = new FileSync('db.json');
+var low = require('lowdb');
+var FileSync = require('lowdb/adapters/FileSync');
+var adapter = new FileSync('db.json');
 
-const db = low(adapter);
+var db = low(adapter);
 
 db.defaults({users: [] })
   .write();
